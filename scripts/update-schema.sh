0068c7324d5b2794835bc335aefde06d565f88d4
dplore <dloher@google.com>
2024-09-25 14:37:32 -0700

    Add OC AFT model 2.7.0

--- scripts/update-schema.sh
+++ scripts/update-schema.sh
@@ -16,6 +16,7 @@
 
 # Hack to ensure that if we are running on OS X with a homebrew installed
 # GNU sed then we can still run sed.
+
 runsed() {
   if hash gsed 2>/dev/null; then
     gsed "$@"
@@ -38,7 +39,8 @@ if [ -z $SRCDIR ]; then
   SRC_DIR=`runreadlink -m ${THIS_DIR}/..`
 fi
 
-proto_generator \
+echo  ${SRC_DIR} 
+./proto_generator \
   -path=${SRC_DIR}/v1/yang,${SRC_DIR}/v1/yang/deps \
   -output_dir=${SRC_DIR}/v1/proto -compress_paths -generate_fakeroot -fakeroot_name=device \
   -package_name=gribi_aft -exclude_modules=ietf-interfaces,openconfig-interfaces \
